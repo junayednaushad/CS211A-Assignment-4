123074b1146290724e8d93e585a0ffca277e1311
Shion Fukuzawa <shionfukuzawa@gmail.com>
2020-12-10 17:49:57 +0900

    First try at Gouraud shader

--- mozilla/src/vertexShader.js
+++ mozilla/src/vertexShader.js
@@ -12,27 +12,46 @@ const vsSource = `
     varying highp vec2 vTextureCoord;
     varying highp vec3 vLighting;
     varying lowp vec4 vColor;
+    varying float vShaderType;
 
     void main() {
         gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
         vColor = aVertexColor;
+        vShaderType = aShaderType;
 
-        if (aShaderType < 0.5) {  // No shading
+        if (aShaderType == 0.0) {  // No shading
             vLighting = vec3(1.0, 1.0, 1.0);
-        } else if (aShaderType == 1.0) { // Gouroud shading
-
+        } else { // Gouroud shading
 
             // Apply lighting effect
-
-            highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-            highp vec3 directionalLightColor = vec3(1, 1, 1);
-            highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+            float k_a = 0.5;
+            float k_d = 1.0;
+            float k_s = 1.0;
+            highp vec4 ambientColor = vec4(0.2, 0.2, 0.2, 1.0);
+            highp vec3 lightColor = vec3(0.9, 0.8, 0.9);
+            highp vec3 specularColor = vec3(0.0, 0.9, 0.9);
+            highp vec3 lightPosition = vec3(4.0, 4.0, 4.0);
+            float shininess = 20.0;
+            float strength = 10.0;
+            lowp vec3 eyeDirection = vec3(0.0, 0.0, -5.0);
 
             highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-
+            highp vec3 directionalVector = normalize(lightPosition);
             highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0); 
-            vLighting = ambientLight + (directionalLightColor * directional);
-        }
+            highp vec3 L_d = lightColor * directional;
+
+            highp vec3 H   = normalize(directionalVector + eyeDirection);
+            highp float specularCoefficient = pow(max(dot(H, transformedNormal.xyz), 0.0), shininess);
+            highp vec3 L_s = specularColor * specularCoefficient * strength;
+
+            highp vec4 GI_a = vec4(k_a * ambientColor.x, k_a * ambientColor.y, k_a * ambientColor.z, 1.0);
+            highp vec4 I_a  = vec4(k_a * ambientColor.x, k_a * ambientColor.y, k_a * ambientColor.z, 1.0);
+            highp vec4 I_d  = vec4(k_d * L_d.x, k_d * L_d.y, k_d * L_d.z, 1.0);
+            highp vec4 I_s  = vec4(L_s * k_s, 1.0);
+
+
+            vLighting = GI_a.xyz + I_d.xyz + I_s.xyz;
+        } 
 
     }
 `;
