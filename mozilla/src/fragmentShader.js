26f131d91e1dd5136fee4aab3514fcf27209c8ee
junayednaushad <junayednaushad@gmail.com>
2020-12-15 02:41:26 -0800

    getting somewhere

--- mozilla/src/fragmentShader.js
+++ mozilla/src/fragmentShader.js
@@ -16,9 +16,9 @@ const fsSource = `
             highp vec3 vAmbientLight = vec3(0.2, 0.2, 0.2);
             highp vec3 vDirectionalLight = vec3(0.9, 0.8, 0.9);
             highp vec3 vL = normalize(vec3(4.0, 4.0, 4.0) - vFragPos.xyz);
-            highp vec3 vV = normalize(vec3(0.0, 0.0, -20.0));
+            highp vec3 vV = normalize(vec3(0.0, 0.0, -20.0) - vFragPos.xyz);
             highp vec3 vH = normalize(vL + vV);
-            highp vec3 vLightingPhong = (vAmbientLight * 0.1) + (vDirectionalLight * max(dot(vNPhong.xyz, vL), 0.0) * 1.0) + (vDirectionalLight * pow(max(dot(vH, vNPhong.xyz), 0.0), 20.0) * 10.0 * 1.0);
+            highp vec3 vLightingPhong = (vAmbientLight * 0.5) + (vDirectionalLight * max(dot(vNPhong.xyz, vL), 0.0) * 1.0) + (vDirectionalLight * pow(max(dot(vH, vNPhong.xyz), 0.0), 20.0) * 10.0);
             gl_FragColor = vec4(vColor.rgb * vLightingPhong, vColor.a);
         }
     }
--- mozilla/src/vertexShader.js
+++ mozilla/src/vertexShader.js
@@ -16,18 +16,18 @@ const vsSource = `
 
     void main() {
         vShaderType = aShaderType;
-        vFragPos = uModelViewMatrix * aVertexPosition;
-        gl_Position = uProjectionMatrix * vFragPos;
+        vFragPos = aVertexPosition;
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
         vColor = aVertexColor;
 
         if (aShaderType == 1.0) {
             highp vec3 vAmbientLight = vec3(0.2, 0.2, 0.2);
             highp vec3 vDirectionalLight = vec3(0.9, 0.8, 0.9);
             highp vec3 vL = normalize(vec3(4.0, 4.0, 4.0) - vFragPos.xyz);
-            highp vec3 vV = normalize(vec3(0.0, 0.0, -20.0));
+            highp vec3 vV = normalize(vec3(0.0, 0.0, -20.0) - vFragPos.xyz);
             highp vec4 vN = uNormalMatrix * vec4(aVertexNormal, 1.0);
             highp vec3 vH = normalize(vL + vV);
-            vLighting = (vAmbientLight * 0.1) + (vDirectionalLight * max(dot(vN.xyz, vL), 0.0) * 1.0) + (vDirectionalLight * pow(max(dot(vH, vN.xyz), 0.0), 20.0) * 10.0 * 1.0);
+            vLighting = (vAmbientLight * 0.5) + (vDirectionalLight * max(dot(vN.xyz, vL), 0.0) * 1.0) + (vDirectionalLight * pow(max(dot(vH, vN.xyz), 0.0), 20.0) * 10.0);
         }
         if (aShaderType == 2.0) {
             vNPhong = uNormalMatrix * vec4(aVertexNormal, 1.0);
